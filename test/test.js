9835464f4f5e8772e73bbf949c033b8c8ce24190
Nik Malev <onescyt@gmail.com>
2017-10-30 20:44:00 +1100

    setting up tests

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect
+var { Game } = require('./game.js')
+var assert = require('assert');
+
+describe('Game', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
