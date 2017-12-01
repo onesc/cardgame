5ca17c133100c61c150e0bae4cef0281e4db6992
Nik Malev <onescyt@gmail.com>
2017-12-02 02:41:58 +1100

    keywords

--- game/deck.js
+++ game/deck.js
@@ -8,6 +8,7 @@ const cards = [
 		text: "", 
 		power: 2, 
 		toughness: 1,
+		keywords: [],
 		imageSrc: "http://2.bp.blogspot.com/_fsMA9Dc4tnA/S-GbvGeOkiI/AAAAAAAAADg/sjDJhE6sF7A/s320/vasily.png"
 	},
 	{
@@ -15,6 +16,7 @@ const cards = [
 		cost: 1, 
 		type: 'Spell', 
 		text: "deal 1 damage to all enemies",
+		keywords: [],
 		imageSrc: "https://hearthstone.gamepedia.com/media/hearthstone.gamepedia.com/thumb/d/de/Arcane_Explosion_full.jpg/500px-Arcane_Explosion_full.jpg?version=30063a3f70632fad015cf27e27f9b5e3",
 		effect: (game, caster) => {
 			var opponent = game.getOpponent(caster.id);
@@ -31,6 +33,7 @@ const cards = [
 		text: "", 
 		power: 3, 
 		toughness: 2,
+		keywords: [],
 		imageSrc: "http://cdn.shopify.com/s/files/1/1058/6186/products/Painted_TrollwithHammer_grande.jpg?v=1467922553"
 	},
 	{
@@ -40,6 +43,7 @@ const cards = [
 		text: "", 
 		power: 1, 
 		toughness: 7,
+		keywords: [],
 		imageSrc: "http://jdillustration.jimmsdesign.co.uk/images/full-scale-image/monster-fat-horror.jpg"
 	},
 	{
@@ -49,6 +53,7 @@ const cards = [
 		text: "whenever your opponent draws a card they take 2 damag", 
 		power: 1, 
 		toughness: 1,
+		keywords: [],
 		imageSrc: "https://orig00.deviantart.net/4fe1/f/2011/343/6/a/apprentice_magician_by_dolphinboy2000-d4in0ie.jpg",
 		eventListeners: [{
 			text: 'whenever your opponent draws a card they take 2 damage',
@@ -66,6 +71,7 @@ const cards = [
 		cost: 2,
 		type: 'Spell',
 		text: "The next combat phase is skipped",
+		keywords: [],
 		imageSrc: "https://www.ecbc.army.mil/news/2012/images/HX-Smoke.jpg",
 		effect: (game) => {
 			game.nextCombatDisabled = true;
@@ -75,7 +81,8 @@ const cards = [
 		name: 'Cute Spiderling', 
 		cost: 3, 
 		type: 'Creature', 
-		text: "Deathtouch", 
+		text: "Deathtouch",
+		keywords: ["Deathtouch"],
 		power: 1, 
 		toughness: 1,
 		imageSrc: "https://ih1.redbubble.net/image.120538650.5332/flat,800x800,075,f.jpg"
@@ -86,6 +93,7 @@ const cards = [
 		type: "Creature",
 		power: 0,
 		toughness: 5,
+		keywords: [],
 		imageSrc: "http://3.bp.blogspot.com/-oLKvUEgLnkE/T0QPWmhuSrI/AAAAAAAAARI/G0usK7iebWc/s1600/fantasy_art_scenery_wallpaper_ognian_bonev_02.jpg",
 		text: "Draw a card whenever you play a creature",
 		eventListeners: [{
@@ -103,6 +111,7 @@ const cards = [
 		cost: 3, 
 		type: 'Spell', 
 		text: "Deal 4 damage to target enemy",
+		keywords: [],
 		imageSrc: "https://hearthstone.gamepedia.com/media/hearthstone.gamepedia.com/thumb/c/cf/Lava_Burst_full.jpg/498px-Lava_Burst_full.jpg?version=c14e366de641391b7772f3b3cc167afe",
 		effect: (game, caster) => {
 			if (caster.target === null) {
@@ -120,6 +129,7 @@ const cards = [
 		cost: 3,
 		type: 'Spell',
 		text: 'Draw two cards',
+		keywords: [],
 		imageSrc: "http://i0.kym-cdn.com/entries/icons/mobile/000/022/266/brain.jpg",
 		effect: (game, caster) => {
 			game.playerDraw(caster.id);
@@ -131,6 +141,7 @@ const cards = [
 		cost: 2, 
 		type: 'Spell',
 		text: 'Put a copy of target creature in to your hand. It costs only one mana to cast.',
+		keywords: [],
 		imageSrc: "http://uforeview.tripod.com/cjimages/teleportation.jpg",
 		effect: (game, caster) => {
 			if (caster.target.type === "Creature") {
@@ -149,6 +160,7 @@ const cards = [
 		toughness: 1,
 		type: 'Creature',
 		text: 'Gains +1 / +1 when it kills a creature',
+		keywords: [],
 		imageSrc: "http://i.imgur.com/Ljg8f4W.png",
 		eventListeners: [{
 			text: 'Troll Headhunter gains +1 / +1 each time it kills a creature',
@@ -169,6 +181,7 @@ const cards = [
 		toughness: 2,
 		type: 'Creature',
 		text: 'Whenever a creature dies deal 2 damage to your opponent',
+		keywords: [],
 		imageSrc: "https://vignette2.wikia.nocookie.net/dragonsdogmaquest/images/d/dd/Skeleton-sorcerer.jpg/revision/latest?cb=20140808082339",
 		eventListeners: [{
 			text: '',
